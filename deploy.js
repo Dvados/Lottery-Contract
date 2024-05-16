19ce961a48d2c9333561407b02de77d5ad7c979e
Vladyslav Tyshchenko <Dvados9989@gmail.com>
2024-05-16 12:53:22 -0600

    Updating to Solc v0.8.9

--- deploy.js
+++ deploy.js
@@ -1,23 +1,24 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
-
+const { abi, evm } = require('./compile');
+ 
 const provider = new HDWalletProvider(
   'ten flush cereal useless mechanic else pink thought cup rather rubber giggle',
   'https://sepolia.infura.io/v3/6ba0a8379821454ead7f0bb65b7274a2'
 );
+ 
 const web3 = new Web3(provider);
-
+ 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
+ 
   console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
-
-  console.log(interface);
+ 
+  console.log(JSON.stringify(abi));
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
